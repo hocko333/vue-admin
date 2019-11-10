8e6d0966b2d5b4d62c257c5ba304fd86cdff389b
cr_mac <chengrui_mars@163.com>
2019-11-11 02:51:50 +0800

    初始化登录页面,准备引入element

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home'
 
 Vue.use(VueRouter)
 
-const routes = [
+const constRoutes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login'),
+    meta: { title: '登录' }
   }
 ]
 
-const router = new VueRouter({
-  routes
+const createRouter = () => new VueRouter({
+  scrollBehavior: () => ({x: 0, y: 0}),
+  routes: constRoutes
 })
+
+const router = createRouter()
+
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher
+}
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  background: url('../assets/bgimg/loginBg.jpg') no-repeat center center;
+  
+}
+</style>
