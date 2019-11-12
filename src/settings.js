820e3d1d8d2eef07b767c1f3d6301afe5b50e1d1
hocko333 <chengkuan689@163.com>
2019-11-12 20:16:02 +0800

    修改mock接口

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,6 @@
+// 'https://www.easy-mock.com/mock/5dc7fcae452b5262380ceb2c/vueadmin' 这个是easy-mock官方
+// 'https://easy-mock.bookset.io/mock/5dca452cbd94c34583b56a89/vue-admin' 这个是别人搭建的分流
 module.exports = {
   baseUrlOfMock:
-    'https://www.easy-mock.com/mock/5dc7fcae452b5262380ceb2c/vueadmin'
+    'https://easy-mock.bookset.io/mock/5dca452cbd94c34583b56a89/vue-admin'
 }
