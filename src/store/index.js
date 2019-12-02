7f3e807309378aa299b2d54cf0efa31098c7cd02
cr_mac <chengrui_mars@163.com>
2019-12-02 16:02:20 +0800

    自动加载store下面的所有modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import user from './modules/user'
 
 Vue.use(Vuex)
+
+// 获取所有模块的路径
+const modulesFiles = require.context('./modules', true, /\.js$/)
+
+// 导入所有模块
+const modules = modulesFiles.keys().reduce((modules, modulesPath) => {
+  const modulesName = modulesPath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const modulesValue = modulesFiles(modulesPath)
+  modules[modulesName] = modulesValue.default
+  return modules
+}, {})
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {
-    user
-  }
+  modules
 })
---
+++ src/store/modules/app.js
@@ -0,0 +1,31 @@
+const isCollapseFromStorage = window.sessionStorage.getItem('isCollapse')
+
+const state = {
+  sideBar: {
+    isCollapse: isCollapseFromStorage ? !!+isCollapseFromStorage : true
+  }
+}
+
+const mutations = {
+  TOGGLE_SIDEBAR: state => {
+    state.sideBar.isCollapse = !state.sideBar.isCollapse
+    if (state.sideBar.isCollapse) {
+      window.sessionStorage.setItem('isCollapse', '1')
+    } else {
+      window.sessionStorage.setItem('isCollapse', '0')
+    }
+  }
+}
+
+const actions = {
+  toggleSidebar({ commit }) {
+    commit('TOGGLE_SIDEBAR')
+  }
+}
+
+export default {
+  namespace: true,
+  state,
+  mutations,
+  actions
+}
