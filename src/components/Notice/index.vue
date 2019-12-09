cdb7ededb563b66593666626183ea67d693b4e18
cr_mac <chengrui_mars@163.com>
2019-12-10 01:05:59 +0800

    新增消息抽屉控件

--- src/components/Notice/index.vue
+++ src/components/Notice/index.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <div class="message item_class">
-      <i class="el-icon-message-solid"></i>
+  <div class="notice-container">
+    <div class="message item_class" @click="handleClickNotice">
+      <el-badge is-dot>
+        <i class="el-icon-message-solid"></i>
+      </el-badge>
     </div>
+    <el-drawer
+      title="我是标题"
+      custom-class="notice_drawer"
+      :size="size"
+      :modal="false"
+      :visible.sync="drawer"
+      :direction="direction"
+      :before-close="handleClose"
+      :wrapperClosable="true"
+    >
+      <span>我来啦!</span>
+    </el-drawer>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      drawer: false,
+      direction: 'rtl',
+      size: '22%'
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.drawer = false
+      done()
+    },
+    handleClickNotice() {
+      this.drawer = !this.drawer
+    }
+  }
 }
 </script>
 
