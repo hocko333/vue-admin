49b527b189d8aa3ebe77c80fbff7e1b243e5bb4d
cr_mac <chengrui_mars@163.com>
2019-12-10 01:57:20 +0800

    实现全屏全换功能

--- src/components/FullScreen/index.vue
+++ src/components/FullScreen/index.vue
@@ -1,13 +1,48 @@
 <template>
   <div class="full-screen-container">
-    <div class="full_screen item_class">
-      <i class="el-icon-full-screen"></i>
+    <div class="full_screen item_class" @click="handleClick">
+      <i :class="isFullScreen ? 'el-icon-aim' : 'el-icon-full-screen'"></i>
     </div>
   </div>
 </template>
 
 <script>
+import screenFull from 'screenfull'
+
 export default {
+  data() {
+    return {
+      isFullScreen: false
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  beforeDestroy() {
+    this.destroy()
+  },
+  methods: {
+    handleClick() {
+      if (!screenFull.isEnabled) {
+        this.$message.warning('抱歉，您的浏览器不支持全屏功能~')
+        return
+      }
+      screenFull.toggle()
+    },
+    change() {
+      this.isFullScreen = screenFull.isFullscreen
+    },
+    init() {
+      if (screenFull.isEnabled) {
+        screenFull.on('change', this.change)
+      }
+    },
+    destroy() {
+      if (screenFull.isEnabled) {
+        screenFull.off('change', this.change)
+      }
+    }
+  }
 }
 </script>
 
